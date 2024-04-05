55f8ca63c5dc5e68e178108f8cd8fb3bf50fb766
cedric <icingerpower@gmail.com>
2024-04-05 08:59:46 +0200

    Added iterator to SortedMap

--- utils/SortedMap.h
+++ utils/SortedMap.h
@@ -1,26 +1,23 @@
 #ifndef SORTEDMAP_H
 #define SORTEDMAP_H
 
+#include <QMap>
+#include <QHash>
+#include <QStringList>
+#include <QtGlobal>
 
-#include <QtCore/qmap.h>
-#include <QtCore/qhash.h>
-#include <QtCore/qstringlist.h>
-#include <QtCore/qglobal.h>
-
-//----------------------------------------------------------
-//----------------------------------------------------------
-//----------------------------------------------------------
-
-//template<typename Key, typename T> class SortedMapIterator;
+/*
+ * Classe qui fonctionne comme un QHash, mais où les éléments sont itérés par ordre d'insertion
+ */
 
 template<typename Key, typename T>
 class SortedMap
 {
-    //friend class SortedMapIterator<Key, T>;
 public:
     SortedMap();
     SortedMap(const SortedMap<Key, T> &other);
     SortedMap(SortedMap<Key, T> &&other);
+    SortedMap(std::initializer_list<std::pair<Key, T>> values);
     ~SortedMap();
     void clear();
     bool contains(const Key &key) const;
@@ -65,31 +62,101 @@ public:
 protected:
     QList<Key> m_sortedKeys;
     QHash<Key, T> m_hash;
-
-
+    
+public:
+    class const_iterator
+    {
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = const std::pair<Key, T>;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const std::pair<Key, T>*;
+        using reference = const std::pair<Key, T>&;
+ 
+        const_iterator(typename QList<Key>::const_iterator keyIt, const QHash<Key, T>& hash)
+            : keyIt(keyIt), hash(hash) {}
+ 
+        const_iterator& operator++() { ++keyIt; return *this; }
+        const_iterator operator++(int) { const_iterator temp(*this); ++keyIt; return temp; }
+        const_iterator& operator--() { --keyIt; return *this; }
+        const_iterator operator--(int) { const_iterator temp(*this); --keyIt; return temp; }
+        bool operator==(const const_iterator& other) const { return keyIt == other.keyIt; }
+        bool operator!=(const const_iterator& other) const { return keyIt != other.keyIt; }
+        const std::pair<Key, T>& operator*() const { return std::make_pair(*keyIt, hash.value(*keyIt)); }
+ 
+        const Key& key() const { return *keyIt; }
+        const T &value() const { return hash.constFind(*keyIt).value(); }
+ 
+    private:
+        typename QList<Key>::const_iterator keyIt;
+        const QHash<Key, T>& hash;
+    };
+    
+    const_iterator constBegin() const { return const_iterator(m_sortedKeys.constBegin(), m_hash); }
+    const_iterator constEnd() const { return const_iterator(m_sortedKeys.constEnd(), m_hash); }
+    
+    class iterator
+    {
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = std::pair<Key, T>;
+        using difference_type = std::ptrdiff_t;
+        using pointer = value_type*;
+        using reference = value_type&;
+        
+        iterator(typename QList<Key>::iterator keyIt, QHash<Key, T>& hash)
+            : keyIt(keyIt), hash(hash) {}
+        
+        iterator& operator++() { ++keyIt; return *this; }
+        iterator operator++(int) { iterator temp(*this); ++keyIt; return temp; }
+        iterator& operator--() { --keyIt; return *this; }
+        iterator operator--(int) { iterator temp(*this); --keyIt; return temp; }
+        bool operator==(const iterator& other) const { return keyIt == other.keyIt; }
+        bool operator!=(const iterator& other) const { return keyIt != other.keyIt; }
+        std::pair<Key, T>& operator*() { return std::make_pair(*keyIt, hash[*keyIt]); }
+        
+        Key& key() { return *keyIt; }
+        T& value() { return hash[*keyIt]; }
+        
+    private:
+        typename QList<Key>::iterator keyIt;
+        QHash<Key, T>& hash;
+    };
+    
+    iterator begin() { return iterator(m_sortedKeys.begin(), m_hash); }
+    iterator end() { return iterator(m_sortedKeys.end(), m_hash); }
 };
-//----------------------------------------------------------
-//----------------------------------------------------------
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 SortedMap<Key, T>::SortedMap()
 {
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 SortedMap<Key, T>::SortedMap(const SortedMap<Key, T> &other)
 {
     m_hash = other.m_hash;
     m_sortedKeys = other.m_sortedKeys;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 SortedMap<Key, T>::SortedMap(SortedMap<Key, T> &&other)
 {
     m_hash = qMove(other.m_hash);
     m_sortedKeys = qMove(other.m_sortedKeys);
 }
-//----------------------------------------------------------
+
+template<typename Key, typename T>
+SortedMap<Key, T>::SortedMap(
+    std::initializer_list<std::pair<Key, T> > values)
+{
+    for (const auto &value : values)
+    {
+        m_sortedKeys << value.first;
+        m_hash[value.first] = value.second;
+    }
+}
+
 template<typename Key, typename T>
 SortedMap<Key, T> &SortedMap<Key, T>::operator=(
         const SortedMap<Key, T> &other)
@@ -98,7 +165,7 @@ SortedMap<Key, T> &SortedMap<Key, T>::operator=(
     m_sortedKeys = other.m_sortedKeys;
     return *this;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 SortedMap<Key, T> &SortedMap<Key, T>::operator=(
         SortedMap<Key, T> &&other)
@@ -107,12 +174,12 @@ SortedMap<Key, T> &SortedMap<Key, T>::operator=(
     m_sortedKeys = qMove(other.m_sortedKeys);
     return *this;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 SortedMap<Key, T>::~SortedMap()
 {
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 bool SortedMap<Key, T>::operator!=(
         const SortedMap<Key, T> &other) const
@@ -122,7 +189,7 @@ bool SortedMap<Key, T>::operator!=(
             || m_sortedKeys != other.m_sortedKeys;
     return different;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 bool SortedMap<Key, T>::operator==(
         const SortedMap<Key, T> &other) const
@@ -132,14 +199,14 @@ bool SortedMap<Key, T>::operator==(
             && m_sortedKeys == other.m_sortedKeys;
     return equals;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 void SortedMap<Key, T>::clear()
 {
     m_sortedKeys.clear();
     m_hash.clear();
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 T &SortedMap<Key, T>::operator[](const Key &key)
 {
@@ -149,59 +216,59 @@ T &SortedMap<Key, T>::operator[](const Key &key)
     }
     return m_hash[key];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const T SortedMap<Key, T>::operator[](const Key &key) const
 {
     return m_hash[key];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 bool SortedMap<Key, T>::contains(const Key &key) const
 {
     bool contains = m_hash.contains(key);
     return contains;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 T SortedMap<Key, T>::take(const Key &key)
 {
     m_sortedKeys.removeOne(key);
     return m_hash.take(key);
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 T &SortedMap<Key, T>::value(const Key &key)
 {
     return m_hash[key];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 bool SortedMap<Key, T>::empty() const
 {
     bool is = m_sortedKeys.isEmpty();
     return is;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 bool SortedMap<Key, T>::isEmpty() const
 {
     bool is = m_sortedKeys.isEmpty();
     return is;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 T &SortedMap<Key, T>::first()
 {
     return m_hash[m_sortedKeys[0]];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const T &SortedMap<Key, T>::first() const
 {
     return m_hash[m_sortedKeys[0]];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 void SortedMap<Key, T>::insert(
         const Key &key, const T &value)
@@ -209,7 +276,7 @@ void SortedMap<Key, T>::insert(
     m_sortedKeys << key;
     m_hash[key] = value;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 void SortedMap<Key, T>::replaceKey(
         const Key &oldKey, const Key &newKey)
@@ -219,7 +286,7 @@ void SortedMap<Key, T>::replaceKey(
     int indexOld = m_sortedKeys.indexOf(oldKey);
     m_sortedKeys[indexOld] = newKey;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const Key SortedMap<Key, T>::key(
         const T &value,
@@ -227,25 +294,25 @@ const Key SortedMap<Key, T>::key(
 {
     return m_hash.key(value, defaultKey);
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const Key SortedMap<Key, T>::keyByIndex(int index) const
 {
     return m_sortedKeys[index];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const Key *SortedMap<Key, T>::keyIt(int index) const
 {
     return &m_sortedKeys[index];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 QList<Key> SortedMap<Key, T>::keys() const
 {
     return m_sortedKeys;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 QList<Key> SortedMap<Key, T>::keys(const T &value) const
 {
@@ -253,25 +320,25 @@ QList<Key> SortedMap<Key, T>::keys(const T &value) const
     keys << key(value);
     return keys;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 T &SortedMap<Key, T>::last()
 {
     return m_hash[m_sortedKeys.last()];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const T &SortedMap<Key, T>::last() const
 {
     return m_hash[m_sortedKeys.last()];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const Key &SortedMap<Key, T>::firstKey() const
 {
     return m_sortedKeys[0];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 int SortedMap<Key, T>::remove(const Key &key)
 {
@@ -279,52 +346,57 @@ int SortedMap<Key, T>::remove(const Key &key)
     int ret = m_hash.remove(key);
     return ret;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 int SortedMap<Key, T>::remove(int index) {
     int ret = remove(m_sortedKeys[index]);
     return ret;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 int SortedMap<Key, T>::size() const
 {
     int size = m_sortedKeys.size();
     return size;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const T SortedMap<Key, T>::valueByIndex(int index) const
 {
     return m_hash[m_sortedKeys[index]];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 T &SortedMap<Key, T>::valueByIndex(int index)
 {
     return m_hash[m_sortedKeys[index]];
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 void SortedMap<Key, T>::setValue(int index, const T &value)
 {
     m_hash[m_sortedKeys[index]] = value;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const T SortedMap<Key, T>::value(
         const Key &key, const T &defaultValue) const
 {
-    return m_hash.value(key, defaultValue);
+    T value = defaultValue;
+    if(m_hash.contains(key))
+    {
+        value = m_hash[key];
+    }
+    return value;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 const T &SortedMap<Key, T>::valueByRef(
         const Key &key) const
 {
     return m_hash.constFind(key).value();
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 QList<T> SortedMap<Key, T>::values() const
 {
@@ -335,7 +407,7 @@ QList<T> SortedMap<Key, T>::values() const
     }
     return values;
 }
-//----------------------------------------------------------
+
 template<typename Key, typename T>
 QList<T> SortedMap<Key, T>::values(const Key &key) const
 {
@@ -343,115 +415,5 @@ QList<T> SortedMap<Key, T>::values(const Key &key) const
     values << value(key);
     return values;
 }
-//----------------------------------------------------------
-//----------------------------------------------------------
-//----------------------------------------------------------
-template<typename Key, typename T>
-class SortedMapIterator
-{
-public:
-    SortedMapIterator(const SortedMap<Key, T> &other);
-    //SortedMapIterator &operator=(
-            //const SortedMap<Key, T> &map);
-    bool hasNext() const;
-    bool hasPrevious() const;
-    const Key key() const;
-    void next();
-    void previous();
-    void toBack();
-    void toFront();
-    const T value() const;
-
-private:
-    const SortedMap<Key, T> *m_map;
-    int m_position;
-};
-//----------------------------------------------------------
-template<typename Key, typename T>
-SortedMapIterator<Key, T>::SortedMapIterator(
-        const SortedMap<Key, T> &other)
-{
-    m_map = &other;
-    m_position = -1;
-}
-
-//----------------------------------------------------------
-/*
-template<typename Key, typename T>
-SortedMapIterator &SortedMapIterator<Key, T>::operator=(
-        const SortedMap<Key, T> &map)
-{
-    m_map = &map;
-    m_position = -1;
-    return *this;
-}
-//*/
-//----------------------------------------------------------
-template<typename Key, typename T>
-bool SortedMapIterator<Key, T>::hasNext() const
-{
-    bool has = (m_position == -1 && m_map->size() > 0)
-            || (m_position < m_map->size() - 1);
-    return has;
-}
-//----------------------------------------------------------
-template<typename Key, typename T>
-bool SortedMapIterator<Key, T>::hasPrevious() const
-{
-    bool has = m_map->size() > 1 && m_position > 0;
-    return has;
-}
-//----------------------------------------------------------
-template<typename Key, typename T>
-const Key SortedMapIterator<Key, T>::key() const
-{
-    return m_map->keyByIndex(m_position);
-}
-//----------------------------------------------------------
-template<typename Key, typename T>
-void SortedMapIterator<Key, T>::next()
-{
-    if(m_position < m_map->size() - 1)
-    {
-        m_position++;
-    }
-}
-//----------------------------------------------------------
-template<typename Key, typename T>
-void SortedMapIterator<Key, T>::previous()
-{
-    if(m_position > 0)
-    {
-        m_position--;
-    }
-}
-//----------------------------------------------------------
-template<typename Key, typename T>
-void SortedMapIterator<Key, T>::toBack()
-{
-    m_position = m_map->size() - 1;
-}
-//----------------------------------------------------------
-template<typename Key, typename T>
-void SortedMapIterator<Key, T>::toFront()
-{
-    if(m_map->size() > 0)
-    {
-        m_position = 0;
-    }
-    else
-    {
-        m_position = -1;
-    }
-}
-//----------------------------------------------------------
-template<typename Key, typename T>
-const T SortedMapIterator<Key, T>::value() const
-{
-    return m_map->value(m_position);
-}
-//----------------------------------------------------------
-//----------------------------------------------------------
-//----------------------------------------------------------
 
 #endif // SORTEDMAP_H
